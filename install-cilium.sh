b50bc52d8cce6865b711ab3176991aca588e5161
胡海龙 <88614993@qq.com>
2021-03-17 11:27:51 +0800

    update cilium version

--- install-cilium.sh
+++ install-cilium.sh
@@ -4,15 +4,13 @@
 helm repo add cilium https://helm.cilium.io/
 
 # Preload the cilium image into each worker node in the kind cluster.
-docker pull quay.io/cilium/cilium:v1.9.4
-docker pull cilium/operator-generic:v1.9.4
-docker pull cilium/startup-script:62bfbe88c17778aad7bef9fa57ff9e2d4a9ba0d8
+docker pull cilium/cilium:v1.9.5
+docker pull cilium/operator-generic:v1.9.5
 
-kind load docker-image quay.io/cilium/cilium:v1.9.4 --name dev
-kind load docker-image cilium/operator-generic:v1.9.4 --name dev
-kind load docker-image cilium/startup-script:62bfbe88c17778aad7bef9fa57ff9e2d4a9ba0d8 --name dev
+kind load docker-image cilium/cilium:v1.9.5 --name dev
+kind load docker-image cilium/operator-generic:v1.9.5 --name dev
 # install 
-helm install cilium cilium/cilium --version 1.9.4 \
+helm install cilium cilium/cilium --version 1.9.5 \
    --namespace kube-system \
    --set nodeinit.enabled=true \
    --set kubeProxyReplacement=partial \
